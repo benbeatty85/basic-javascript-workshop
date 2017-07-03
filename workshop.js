9cf5d8b78f31c19887e33fd443ae0f52e69df23a
Ben Beatty <benbeatty85@gmail.com>
2017-07-03 23:54:13 +0000

    completed sumOfNumbers function

--- workshop.js
+++ workshop.js
@@ -109,7 +109,20 @@ function capitalize(inputString) {
 }
 
 function sumOfNumbers(arrayOfNumbers) {
-
+    if (typeof arrayOfNumbers !== "object") {
+        return;
+    }
+    for (var i = 0; i <arrayOfNumbers.length; i ++) {
+        if (typeof arrayOfNumbers[i] !== "number") {
+            return;
+        }
+    }
+    
+    var temp = 0;
+    for (var i = 0; i < arrayOfNumbers.length; i ++) {
+        temp = temp + arrayOfNumbers[i];
+    }
+    return temp;
 }
 
 function uniqueElements(array1, array2) {
